f772f93de89b19d9b48c8456bc1734973a2c278a
Nick Matantsev <nick.m@myplanetdigital.com>
2015-04-08 14:34:34 -0400

    Using zone.js to detect "interesting events"

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-var $ = require('jquery');
 var requestAnimationFrame = require('raf');
 var diff = require('virtual-dom/diff');
 var patch = require('virtual-dom/patch');
 var createElement = require('virtual-dom/create-element');
 var h = require('virtual-dom/h');
+var zoneWrapper = require('zone.js');
 
 // explicit method to trigger refresh
 // (may be using a private jQuery instance so must use it to trigger event)
 window.vdomLiveRefresh = function () {
-    $(window).trigger('vdomlive:refresh');
+    // @todo get rid of this?
 };
 
 module.exports = function (zoneCode) {
-    var factory = function (render) {
-        var cleanup = null;
+    var redrawList = [];
 
+    var factory = function (render) {
         var tree = render(h);
         var rootNode = createElement(tree);
 
         var redrawId = null;
 
         function requestRedraw() {
-            if (!rootNode.parentNode) {
-                cleanup();
-                return;
-            }
-
             if (redrawId === null) {
                 redrawId = requestAnimationFrame(function () {
                     redrawId = null;
+
+                    // clean up
+                    if (!rootNode.parentNode) {
+                        redrawList.splice(redrawList.indexOf(requestRedraw), 1);
+                        return;
+                    }
 
                     var newTree = render(h);
                     patch(rootNode, diff(tree, newTree));
@@ -37,18 +38,23 @@ module.exports = function (zoneCode) {
             }
         }
 
-        $(window).on('vdomlive:refresh', requestRedraw);
-        $(window).on('hashchange', requestRedraw);
-        $(document.body).on('click', requestRedraw);
-
-        cleanup = function () {
-            $(window).off('vdomlive:refresh', requestRedraw);
-            $(window).off('hashchange', requestRedraw);
-            $(document.body).off('click', requestRedraw);
-        };
+        redrawList.push(requestRedraw);
 
         return rootNode;
     };
 
-    zoneCode(factory);
+    var currentZoneIsInitialized = false;
+    var currentZone = zoneWrapper.zone.fork({
+        afterTask: function () {
+            if (currentZoneIsInitialized) {
+                redrawList.forEach(function (hook) { hook(); });
+            }
+        }
+    });
+
+    currentZone.run(function () {
+        zoneCode(factory);
+    });
+
+    currentZoneIsInitialized = true;
 };
